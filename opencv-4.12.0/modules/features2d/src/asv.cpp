01b4cb2d31531991f35c8ace607462822a80f82f
Matthew Kim <matt@ganderworks.com>
2025-11-28 12:46:28 -0800

    updated extractMultiScaleDescriptors

--- opencv-4.12.0/modules/features2d/src/asv.cpp
+++ opencv-4.12.0/modules/features2d/src/asv.cpp
@@ -1,91 +1,99 @@
-/** @brief Class for computing descriptors using Accumulated Stability Voting 
- (ASV) algorithm based on: Yang, T.-Y., Lin, Y.-Y. & Chuang, Y.-Y., 
- "Accumulated Stability Voting: A Robust Descriptor from Descriptors of 
+/** @brief Class for computing descriptors using Accumulated Stability Voting
+ (ASV) algorithm based on: Yang, T.-Y., Lin, Y.-Y. & Chuang, Y.-Y.,
+ "Accumulated Stability Voting: A Robust Descriptor from Descriptors of
  Multiple Scales," CVPR 2016.
 
-ASV calculates descriptors at multiple scales and uses stability voting to 
-create more better descriptors. The ASV descriptor can work with any Feature2D 
+ASV calculates descriptors at multiple scales and uses stability voting to
+create more better descriptors. The ASV descriptor can work with any Feature2D
 detector (SIFT, ORB, BRISK, ...)
 */
 
-#include "precomp.hpp"
-#include "opencv2/features2d.hpp"
-#include <numeric>
 #include <iostream>
+#include <numeric>
+
+#include "opencv2/features2d.hpp"
+// #include "precomp.hpp"
 
 namespace cv {
 
-    // constructor
-    ASV::ASV(const Ptr<Feature2D>& _detector,
-            int _nScales,
-            double _scaleStep,
-            double _nThreshold1,
-            double _nThreshold2,
-            bool _isInter)
-        : detector(_detector),
-        nScales(_nScales),
-        scaleStep(_scaleStep),
-        nThreshold1(_nThreshold1),
-        nThreshold2(_nThreshold2),
-        isInter(_isInter)
-    {
-    }
-
-    Ptr<ASV> ASV::create(const Ptr<Feature2D>& detector,
-                        int nScales,
-                        double scaleStep,
-                        double nThreshold1,
-                        double nThreshold2,
-                        bool isInter)
-    {
-        return makePtr<ASV>(detector, nScales, scaleStep, nThreshold1, nThreshold2, isInter);
-    }
-
-    // compute descriptors
-    void ASV::compute(InputArray _image, std::vector<KeyPoint>& keypoints,
-                    OutputArray _descriptors, OutputArray _binaryDescriptors)
-    {
-        CV_Assert(!detector.empty());
-        
-        Mat image = _image.getMat();
-
-        if (keypoints.empty())
-        {
-            _descriptors.release();
-            CV_Error( Error::StsBadArg, "No keypoints detected." );
-        }
-
-        // store multiscale descriptors as vector [keypoint][scale][Mat]
-        int nKeypoints = (int)keypoints.size();
-        std::vector<std::vector<Mat>> multiScaleDescriptors(nKeypoints);
-        
-        // extract descriptors at multiple scales around each keypoint
-        for (int scaleIdx = 0; scaleIdx < nScales; scaleIdx++)
-        {
-            // create scaled keypoints copy
-            double scaleFactor = std::pow(scaleStep, scaleIdx - nScales / 2.0);            
-            std::vector<KeyPoint> scaledKeypoints = keypoints; // deep copy keypoints
-            for (size_t i = 0; i < nKeypoints; i++)
-            {
-                scaledKeypoints[i].size *= static_cast<float>(scaleFactor);
-            }
-
-            // get descriptors at this scale
-            Mat scaledDescriptor; // [keypoint][descriptor]
-            detector->compute(image, scaledKeypoints, scaledDescriptor);
-
-            if (nKeypoints > scaledDescriptor.rows) {
-                std::cerr << "nKeypoints > scaledDescriptor.rows" << std::endl;
-                exit(1);
-            }
-
-            // store descriptors for each keypoint
-            for (int i = 0; i < nKeypoints; i++)
-            {                
-                multiScaleDescriptors[i].push_back(scaledDescriptor.row(i).clone());
-            }
-        }
-        
-        // compute stability voting on multiScaleDescriptors
-    }
+// constructor
+ASV::ASV(const int detectorType = 0, const int _nScales,
+         const double _scaleStep, const double _nThreshold1,
+         const double _nThreshold2, const bool _isInter, )
+    : nScales(_nScales),
+      scaleStep(_scaleStep),
+      nThreshold1(_nThreshold1),
+      nThreshold2(_nThreshold2),
+      isInter(_isInter) {
+  detector = SIFT::create(contrastThreshold = 0,
+                          edgeThreshold = 0);  // default detector
 }
+
+Ptr<ASV> ASV::create(const int detectorType, int nScales, double scaleStep,
+                     double nThreshold1, double nThreshold2, bool isInter) {
+  return makePtr<ASV>(nScales, scaleStep, nThreshold1, nThreshold2, isInter,
+                      detectorType);
+}
+
+// compute descriptors
+void ASV::compute(InputArray _image, std::vector<KeyPoint>& keypoints,
+                  OutputArray _descriptors, OutputArray _binaryDescriptors) {
+  CV_Assert(!detector.empty());
+
+  Mat image = _image.getMat();
+
+  if (keypoints.empty()) {
+    _descriptors.release();
+    CV_Error(Error::StsBadArg, "No keypoints detected.");
+  }
+
+  // store multiscale descriptors as vector [keypoint][scale][Mat]
+  std::vector<std::vector<Mat>> multiScaleDescriptors;
+  extractMultiScaleDescriptors(image, keypoints, multiScaleDescriptors, );
+
+  // compute stability voting on multiScaleDescriptors
+}
+
+// extract descriptors at multiple scales around each keypoint
+// Output: multiScaleDescriptors [keypoint][scale][Mat]
+void ASV::extractMultiScaleDescriptors(
+    const Mat& image, const std::vector<KeyPoint>& keypoints,
+    std::vector<std::vector<Mat>>& multiScaleDescriptors) {
+  int nKeypoints = (int)keypoints.size();
+  multiScaleDescriptors.clear();
+  multiScaleDescriptors.resize(nKeypoints);
+
+  // extract multiple descriptors per keypoint
+  for (int i = 0; i < nKeypoints; i++) {
+    std::vector<Mat> scaledDescriptors;  // [descriptor per scale]
+    bool isValidKeypoint = true;
+    scaledDescriptors.reserve(nScales);
+
+    // generate scale space and extract descriptors
+    for (int scaleIdx = 0; scaleIdx < nScales; scaleIdx++) {
+      KeyPoint scaledKeypoint = keypoints[i];  // deep copy keypoint
+      double scaleFactor = std::pow(scaleStep, scaleIdx - nScales / 2.0);
+      scaledKeypoint.size *= static_cast<float>(scaleFactor);
+
+      Mat descriptor;
+      std::vector<KeyPoint> oneKp{scaledKeypoint};  // compute needs a vector
+      detector->compute(image, oneKp, descriptor);
+
+      if (descriptor.empty()) {
+        isValidKeypoint = false;
+        break;
+      }
+
+      scaledDescriptors.push_back(descriptor);
+    }
+
+    // skip keypoints where descriptors could not be computed at all scales
+    // check with multiScaleDescriptors[i].empty() later
+    if (!isValidKeypoint) {
+      continue;
+    }
+
+    multiScaleDescriptors[i] = std::move(scaledDescriptors);
+  }
+}
+}  // namespace cv
